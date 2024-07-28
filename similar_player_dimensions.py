cb4ac9ad14619bb1cb3c75db88323d1e8e9c7435
Daniel Liu <danielliu2707@gmail.com>
2024-07-28 15:43:02 +1000

    Add documentation to similar_player_dimensions model.
    This commit introduces some documentation to the model that determines the most similar player given the user inputs.

--- similar_player_dimensions.py
+++ similar_player_dimensions.py
@@ -2,10 +2,29 @@ import pandas as pd
 import numpy as np
 
 class SimilarPlayerDimensions:
+    """
+    Class used to predict the most similar player given physical dimensions
+    """
     def __init__(self, active_players: pd.DataFrame):
         self.active_players = active_players
     
-    def predict_similar_player(self, user_height: int, user_weight: int, user_BMI: int, user_pos_prediction: str):
+    def predict_similar_player(self, user_height: int, user_weight: int, user_BMI: int, user_pos_prediction: str) -> pd.Series:
+        """
+        This function determines the most similar NBA player in 2023 to the physical dimensions provided
+        by the user as input. It goes about this by firstly, filtering for active NBA players with the predicted
+        position (G, F, C). Then, compute the cosine similiarty between those filtered active NBA players and the user
+        input attributes. Finally, keep only the active NBA player with the greatest cosine similiarity score to have
+        their name and image outputted in the application.
+
+        Args:
+            user_height (int): User height input
+            user_weight (int): User weight input
+            user_BMI (int): User bmi winput
+            user_pos_prediction (str): Predicted position of user
+
+        Returns:
+            pd.Series: A pandas series containing relevant information about the most similar active NBA player.
+        """
         # Filter for predicted position:
         active_pos_players = self.active_players[self.active_players['position'] == user_pos_prediction]
         
@@ -24,9 +43,6 @@ class SimilarPlayerDimensions:
         cosine_res = cosine_similarity(active_pos_players_num, user_features)
         active_pos_players['cosine_similarity'] = np.squeeze(cosine_res)
         
-        # Find most similar player
-        most_similar_player = active_pos_players[active_pos_players['cosine_similarity'] == np.max(np.squeeze(cosine_res))]
+        # Find most similar player, with iloc handling multiple 'most similar' players
+        most_similar_player = active_pos_players[active_pos_players['cosine_similarity'] == np.max(np.squeeze(cosine_res))].iloc[0, :]
         return most_similar_player
-
-
-        
