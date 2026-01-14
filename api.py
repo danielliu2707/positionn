969fa051c650955f97753ff17f7757d67b3d0672
Daniel Liu <danielliu2707@gmail.com>
2026-01-14 20:49:26 +0800

    Remove Deta Space configuration files

--- api.py
+++ api.py
@@ -198,6 +198,4 @@ def predict_from_dimensions(payload: DimensionsInput):
 
 if __name__ == "__main__":
     import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
